384f4ffaaed58b1e70257c57d713086099808576
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-14 10:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if(s1 == '' && s2 != ''){
+		return -1
+	}
+	let str1 = s1.toLowerCase();
+	let str2 = s2.toLowerCase();
+	let arr = [];
+	if(str1.includes(str2)){
+		arr = str1.split(str2);
+	}
+	else{
+		return -1;
+	}
+	return arr[0].length;
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
